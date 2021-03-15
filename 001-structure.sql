b8469be5e28f4858708be9ad36e1bdc96841fb49
widhaprasa <widhaprasa@gmail.com>
2021-03-15 12:04:44 +0700

    - Fix used group variable name

--- 001-structure.sql
+++ 001-structure.sql
@@ -1,14 +1,8 @@
--- Create database first
-CREATE DATABASE vmq_mysql;
-
--- Use database
-USE DATABASE vmq_mysql;
-
 -- Create default table
 CREATE TABLE vmq_auth_acl
 (
   mountpoint VARCHAR(10) NOT NULL,
-  group VARCHAR(128) NOT NULL,
+  group_ VARCHAR(128) NOT NULL,
   username VARCHAR(128) NOT NULL,
   password VARCHAR(128),
   publish_acl TEXT,
@@ -18,4 +12,4 @@ CREATE TABLE vmq_auth_acl
 
 -- Add index for mountpoint and group
 ALTER TABLE `vmq_auth_acl` INDEX `idx_mountpoint` ON (`mountpoint`);
-ALTER TABLE `vmq_auth_acl` INDEX `idx_group` ON (`group`);
+ALTER TABLE `vmq_auth_acl` INDEX `idx_group` ON (`group_`);
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-"# vernemq-mysql" 
-"# vernemq-mysql" 
+"# vernemq-mysql"
